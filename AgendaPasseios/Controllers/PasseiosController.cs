45359fb5824f52ac139d5b8f6bc0c58f2bc01867
monique <moniqueohana@yahoo.com.br>
2024-12-09 17:08:54 -0300

    feat: Index

---
+++ AgendaPasseios/Controllers/PasseiosController.cs
@@ -0,0 +1,27 @@
+﻿using AgendaPasseios.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaPasseios.Controllers
+{
+    public class PasseiosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Passeio> passeios = new List<Passeio>
+            {
+                new Passeio
+                {
+                    Id = 1,
+                    Nome = "Passeio das águas",
+                    Destino = "Natal",
+                    Valor = 50.00,
+                    Empresa = "Brisa e Mar Turismo"
+
+                }
+
+            }; 
+
+            return View(passeios);
+        }
+    }
+}
