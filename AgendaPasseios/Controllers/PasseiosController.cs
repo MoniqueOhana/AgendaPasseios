462d4779c2f5dd1a53ac8c70438920af07a677a7
monique <moniqueohana@yahoo.com.br>
2024-12-12 11:39:02 -0300

    feat: editar o passeio

--- AgendaPasseios/Controllers/PasseiosController.cs
+++ AgendaPasseios/Controllers/PasseiosController.cs
@@ -2,6 +2,7 @@
 using AgendaPasseios.Models;
 using AgendaPasseios.Services;
 using AgendaPasseios.Services.Exceptions;
+using Humanizer.Localisation;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -66,6 +67,45 @@ namespace AgendaPasseios.Controllers
                 return RedirectToAction(nameof(Index));
             }
             catch (IntegrityException ex)
+            {
+                return RedirectToAction(nameof(Error), new { message = ex.Message });
+            }
+        }
+            
+            public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não fornecido" });
+            }
+            Passeio passeio = await _service.FindByIdAsync(id.Value);
+            if (passeio is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não encontrado" });
+            }
+            return View(passeio);
+        }
+        // POST Genres/Edit/x
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Passeio passeio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (id != passeio.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id's não condizentes" });
+            }
+
+            try
+            {
+                await _service.UpdateAsync(passeio);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException ex)
             {
                 return RedirectToAction(nameof(Error), new { message = ex.Message });
             }
--- AgendaPasseios/Services/PasseioService.cs
+++ AgendaPasseios/Services/PasseioService.cs
@@ -2,6 +2,7 @@
 using AgendaPasseios.Data;
 using AgendaPasseios.Models;
 using AgendaPasseios.Services.Exceptions;
+using Humanizer.Localisation;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
 
@@ -44,5 +45,28 @@ namespace AgendaPasseios.Services
                 throw new IntegrityException(ex.Message);
             }
         }
+        // POST: Genres/Edit/x
+        public async Task UpdateAsync(Passeio passeio)
+        {
+            // Confere se tem alguém com esse Id
+            bool hasAny = await _context.Passeios.AnyAsync(x => x.Id == passeio.Id);
+            // Se não tiver, lança exceção de NotFound.
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id não encontrado");
+            }
+            // Tenta atualizar
+            try
+            {
+                _context.Update(passeio);
+                await _context.SaveChangesAsync();
+            }
+            // Se não der, captura a exceção lançada
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new DbConcurrencyException(ex.Message);
+            }
+        }
+        
     }
 }
